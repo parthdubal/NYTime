8ddb4045783138cf3aa137f09b89120afd9e1a62
Parth Dubal <parthdubal29@gmail.com>
2020-02-22 17:38:27 +0530

    Initial Commit

---
+++ NYTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTime
+//
+//  Created by Parth Dubal on 22/02/20.
+//  Copyright © 2020 Parth Dubal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
